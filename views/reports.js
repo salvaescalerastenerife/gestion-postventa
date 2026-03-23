27bb61df0f6371b561244ee370b3eb761ec39354
salvaescalerastenerife <salvaescaleras.tenerife@gmail.com>
2026-03-23 22:07:12 +0000

    Create reports.js

---
+++ views/reports.js
@@ -0,0 +1,10 @@
+export function viewReports() {
+  return `
+    <div class="view">
+      <div class="card">
+        <h2>Informes</h2>
+        <p>Vista en preparación.</p>
+      </div>
+    </div>
+  `;
+}
